ef1ba12e55b047d54e4a32f808b1bf76fb933312
[Darrick Fauvel] <[darrick@darrickdevelops.com]>
2024-06-23 19:40:26 -0400

    Complete Beginner Series #2 Clock

---
+++ 8kyu/beginner-series-2-clock/main.js
@@ -0,0 +1,13 @@
+function past(h, m, s) {
+  const computedMs = {
+    hour: 60 * 60 * 1000,
+    minute: 60 * 1000,
+    second: 1000,
+  }
+
+  return h * computedMs.hour + m * computedMs.minute + s * computedMs.second
+}
+
+console.log(past(0, 1, 1))
+
+export { past }
---
+++ 8kyu/beginner-series-2-clock/main.test.js
@@ -0,0 +1,13 @@
+import { assert } from "chai"
+
+import { past } from "./main.js"
+
+describe("Beginner Series #2 Clock", () => {
+  it("Tests", () => {
+    assert.strictEqual(past(0, 1, 1), 61000)
+    assert.strictEqual(past(1, 1, 1), 3661000)
+    assert.strictEqual(past(0, 0, 0), 0)
+    assert.strictEqual(past(1, 0, 1), 3601000)
+    assert.strictEqual(past(1, 0, 0), 3600000)
+  })
+})
